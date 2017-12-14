92b81c72475a176337a682b8c71f41f17656f703
Adam Sharp <adam@sharplet.me>
2017-12-14 13:58:55 -0500

    Add setThreshold function and light up LED

--- src/beavis.ino
+++ src/beavis.ino
@@ -1,15 +1,31 @@
 int led = D7;
 int sensor = A0;
 int lightLevel = 0;
+int threshold = 3200;
+bool occupied = false;
 
 void setup() {
   pinMode(led, OUTPUT);
   pinMode(sensor, INPUT);
 
+  Particle.function("setThreshold", setThreshold);
   Particle.variable("lightLevel", lightLevel);
+  Particle.variable("occupied", occupied);
 }
 
 void loop() {
   lightLevel = analogRead(sensor);
+  occupied = lightLevel > threshold;
+  digitalWrite(led, occupied ? HIGH : LOW);
   delay(100);
+}
+
+int setThreshold(String thresholdString) {
+  int newThreshold = thresholdString.toInt();
+  if (newThreshold > 0) {
+    threshold = newThreshold;
+    return threshold;
+  } else {
+    return -1;
+  }
 }
